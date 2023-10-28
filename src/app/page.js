6747086360e09a0522e34ec3e3d1d53303e23754
JRH89 <thegemlins@gmail.com>
2023-10-27 21:24:57 -0700

    new section

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,10 @@ const videos = [
   "https://firebasestorage.googleapis.com/v0/b/hookerhillstu.appspot.com/o/videos%2F10-9_2..mp4?alt=media&token=6f98b494-703e-4e69-9d04-0c4e9a34120d",
 ]
 
+const games = [
+  "https://firebasestorage.googleapis.com/v0/b/hookerhillstu.appspot.com/o/Unreal%20Engine%205%202023.10.16%20-%2016.48.37.01.mp4?alt=media&token=235516a3-540e-4ad0-a7af-df0227e4d2cb"
+]
+
 const images = [
   "/images/Interior_1.png",
   "/images/003.png",
@@ -27,6 +31,7 @@ const images = [
 export default function Home() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0)
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0)
+  const [currentGameIndex, setCurrentGameIndex] = useState(0)
 
   const previousImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length)
@@ -44,15 +49,24 @@ export default function Home() {
     setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videos.length)
   }
 
+  const previousGame = () => {
+    setCurrentGameIndex((prevIndex) => (prevIndex - 1 + games.length) % games.length)
+  }
+
+  const nextGame = () => {
+    setCurrentGameIndex((prevIndex) => (prevIndex + 1) % games.length)
+  }
+
   return (
     <div className="bg-neutral-950 pt-2">
       {/* Hero Section */}
-      <section className="bg-gray-900 rounded-md text-white pb-10 pt-5 max-w-2xl mx-auto">
+      <section className="bg-gray-900 rounded-md text-white pb-5 pt-3 max-w-2xl mx-auto">
         <div className="container mx-auto text-center">
           <h1 className="text-4xl font-bold mb-4">Jared Robert Hooker</h1>
-          <p className="text-lg mb-8">
-            Welcome to my collection of 3D models and video animations created with Blender and Unreal Engine 5.
+          <p className="text-md mb-8">
+            I specialize in using Blender and Unreal Engine to create outstanding 3D models, immersive environments, and captivating animated videos. Explore my portfolio to witness the seamless fusion of technology and creativity.
           </p>
+
 
           <Link legacyBehavior href="https://calendly.com/jaredhooker/30min">
             <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Schedule Interview</a>
@@ -61,9 +75,10 @@ export default function Home() {
       </section>
 
       {/* Image Gallery Section */}
-      <section className="py-5 px-2 bg-gray-800 text-white rounded-t max-w-2xl mx-auto">
+      <section className="py-5
+       px-2 border-b-2 mt-2 border-neutral-950 bg-gray-800 text-white rounded-t max-w-2xl mx-auto">
         <div className="container mx-auto text-center">
-          <h2 className="text-2xl font-bold mb-4">Image Gallery</h2>
+          <h2 className="text-2xl font-bold mb-4">Images</h2>
           <div className="flex justify-center">
             <button className="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2" onClick={previousImage}>
               Previous
@@ -85,9 +100,9 @@ export default function Home() {
       </section>
 
       {/* Video Gallery Section */}
-      <section className="py-5 px-2 bg-gray-800 text-white rounded-b max-w-2xl mx-auto">
+      <section className="py-10 px-2 bg-gray-800 text-white border-b-2 border-neutral-950 max-w-2xl mx-auto">
         <div className="container mx-auto text-center">
-          <h2 className="text-2xl font-bold mb-4">Video Gallery</h2>
+          <h2 className="text-2xl font-bold mb-4">Videos</h2>
           <div className="flex justify-center">
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 w-full" onClick={previousVideo}>
               Previous
@@ -104,6 +119,35 @@ export default function Home() {
           </div>
         </div>
       </section>
+      {/* Game Gallery Section */}
+      <section className="py-10 px-2 bg-gray-800 text-white rounded-b max-w-2xl mx-auto">
+        <div className="container mx-auto text-center">
+          <h2 className="text-2xl font-bold mb-4">Games</h2>
+          <div className="flex justify-center">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2 w-full" onClick={previousGame}>
+              Previous
+            </button>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full" onClick={nextGame}>
+              Next
+            </button>
+          </div>
+          <div className="my-4">
+            <video key={currentGameIndex} preload="metadata" width={1920} height={1080} controls className="rounded-md w-full max-w-2xl mx-auto">
+              <source src={games[currentGameIndex]} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
+      </section>
+      <footer className='py-2'>
+        <div className="bg-neutral-950 text-center duration-200">
+          <a href="https://www.hookerhillstudios.com" class="hover-link transition duration-300">
+            <span class="word">Hooker </span>
+            <span class="word">Hill </span>
+            <span class="word">Studios</span>
+          </a>
+        </div>
+      </footer>
     </div>
   )
 }
